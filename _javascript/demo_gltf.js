94fcc6509ade62fbb441c880fd4617b4d5430a68
Giamo VDE <giamoeerenbeemt@gmail.com>
2019-11-27 13:54:21 +0100

    Auto stash before merge of "master" and "origin/master"

--- _javascript/demo_gltf.js
+++ _javascript/demo_gltf.js
@@ -5,12 +5,13 @@ const SETTINGS = {
   // 'DamagedHelmet/glTF/DamagedHelmet.gltf',
   // 'Charmander/glTF/model.gltf',
   // 'EnglishHat/glTF/tophat.gltf',
-  gltfModelURL:'models/jeeliz/headphones/scene.gltf',
+  gltfModelURL: 'models/jeeliz/headphones/scene.gltf',
   //gltfModelURL: 'models/jeeliz/HeadphoneEars/glTF/headphone_with_ears.glb',
   faceMeshGltfModelURL: 'models/jeeliz/HeadTest/glTF/head.glb',
   cubeMapURL: 'models/jeeliz/Bridge2/',
   offsetYZ: [0.3, 0], //offset of the model in 3D along vertical and depth axis
-  scale: 2.5 //width in 3D of the GLTF model
+  scale: 2.5, //width in 3D of the GLTF model
+  maxFaces: 4 //max number of detected faces
 };
 
 var THREECAMERA = null;
@@ -47,14 +48,14 @@ function init_threeScene(spec) {
     //const bbox = new THREE.Box3().expandByObject(gltf.scene);
     var bbox = new THREE.Box3().setFromObject(gltf.scene);
     console.log(bbox.getSize());
+
     // scale the model according to its width:
     var sizeX = bbox.getSize(new THREE.Vector3()).x;
     gltf.scene.scale.multiplyScalar(SETTINGS.scale / sizeX * 0.65);
+
     //gltf.scene.position.add(new THREE.Vector3(0, SETTINGS.offsetYZ[0], SETTINGS.offsetYZ[1]));
     gltf.scene.position.add(new THREE.Vector3(0, 0, -1));
     bbox = new THREE.Box3().setFromObject(gltf.scene);
-
-
 
     // center the model:
     const centerBBox = bbox.getCenter(new THREE.Vector3());
@@ -62,11 +63,18 @@ function init_threeScene(spec) {
 
     //gltf.scene.rotateX(160);
     //gltf.scene.rotateY(1.57);
+
+    // Multiple faces
+    // threeStuffs.scene.maxFacesDetected = SETTINGS.maxFacesDetected;
+
     // CREATE LIGHT
     const ambientLight = new THREE.AmbientLight(0XFFFFFF, 1);
     threeStuffs.scene.add(ambientLight);
-    // dispatch the model:
-    threeStuffs.faceObject.add(gltf.scene);
+
+    // Display scenes
+    threeStuffs.faceObjects.forEach(function (faceObject) { //display the cube for each detected face
+      faceObject.add(gltf.scene.clone());
+    });
   }); //end gltfLoader.load callback
 
 
@@ -99,11 +107,15 @@ function init_threeScene(spec) {
 
     //gltf.scene.rotateX(160);
     //gltf.scene.rotateY(-1.57);
+
     // CREATE LIGHT
     const ambientLight = new THREE.AmbientLight(0XFFFFFF, 1);
     threeStuffs.scene.add(ambientLight);
+
     // dispatch the model:
-    threeStuffs.faceObject.add(gltf.scene);
+    threeStuffs.faceObjects.forEach(function (faceObject) { //display the cube for each detected face
+      faceObject.add(gltf.scene.clone());
+    });
   }); //end gltfLoader.load callback
 
 
@@ -114,7 +126,7 @@ function init_threeScene(spec) {
 
 //entry point, launched by body.onload():
 function main() {
-  document.getElementById('ar-container').style.display = 'block'; 
+  document.getElementById('ar-container').style.display = 'block';
   JeelizResizer.size_canvas({
     canvasId: 'jeeFaceFilterCanvas',
     isFullScreen: true,
@@ -126,88 +138,87 @@ function main() {
 }
 
 function start() {
-  if(typeof JEEFACEFILTERAPI.toggle_pause(false, true) == 'undefined'){
-  JEEFACEFILTERAPI.init({
-    videoSettings: { //increase the default video resolution since we are in full screen
-      'idealWidth': 1280,  //ideal video width in pixels
-      'idealHeight': 800, //ideal video height in pixels
-      'maxWidth': 1920,   //max video width in pixels
-      'maxHeight': 1920   //max video height in pixels
-    },
-    followZRot: true,
-    canvasId: 'jeeFaceFilterCanvas',
-    NNCpath: 'lib/dist/', //root of NNC.json file
-    callbackReady: function (errCode, spec) {
-      if (errCode) {
-        console.log('AN ERROR HAPPENS. SORRY BRO :( . ERR =', errCode);
-        return;
+  if (typeof JEEFACEFILTERAPI.toggle_pause(false, true) == 'undefined') {
+    JEEFACEFILTERAPI.init({
+      videoSettings: { //increase the default video resolution since we are in full screen
+        'idealWidth': 1280,  //ideal video width in pixels
+        'idealHeight': 800, //ideal video height in pixels
+        'maxWidth': 1920,   //max video width in pixels
+        'maxHeight': 1920   //max video height in pixels
+      },
+      followZRot: true,
+      canvasId: 'jeeFaceFilterCanvas',
+      NNCpath: 'lib/dist/', //root of NNC.json file
+      maxFacesDetected: SETTINGS.maxFaces,
+      callbackReady: function (errCode, spec) {
+        if (errCode) {
+          console.log('AN ERROR HAPPENS. SORRY BRO :( . ERR =', errCode);
+          return;
+        }
+
+        console.log('INFO : JEEFACEFILTERAPI IS READY');
+        init_threeScene(spec);
+      }, //end callbackReady()
+
+      //called at each render iteration (drawing loop):
+      callbackTrack: function (detectState) {
+        THREE.JeelizHelper.render(detectState, THREECAMERA);
       }
+    }); //end JEEFACEFILTERAPI.init call
+    //scroll to top of screen
 
-      console.log('INFO : JEEFACEFILTERAPI IS READY');
-      init_threeScene(spec);
-    }, //end callbackReady()
-
-    //called at each render iteration (drawing loop):
-    callbackTrack: function (detectState) {
-      THREE.JeelizHelper.render(detectState, THREECAMERA);
-    }
-  }); //end JEEFACEFILTERAPI.init call
-  //scroll to top of screen
-
-}
-window.scrollTo(0, 0);
-//end start()
+  }
+  window.scrollTo(0, 0);
+  //end start()
 }
 function screenCapture() {
   var url = jeeFaceFilterCanvas.toDataURL('image/png');
   document.getElementById('screencapture-img').src = url;
   stop()
-  document.getElementById('share-container').style.display = 'block'; 
+  document.getElementById('share-container').style.display = 'block';
 
 }
 
 function dataURLtoFile(dataurl, filename) {
- 
+
   var arr = dataurl.split(','),
-      mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]), 
-      n = bstr.length, 
-      u8arr = new Uint8Array(n);
-      
-  while(n--){
-      u8arr[n] = bstr.charCodeAt(n);
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
   }
-  
-  return new File([u8arr], filename, {type:mime});
+
+  return new File([u8arr], filename, { type: mime });
 }
 
 function shareImage() {
-  var file = dataURLtoFile(document.getElementById('screencapture-img').src,'image.png');
-console.log(file);
-  var filesArray = []; 
+  var file = dataURLtoFile(document.getElementById('screencapture-img').src, 'image.png');
+  console.log(file);
+  var filesArray = [];
   filesArray.push(file);
-  if (navigator.canShare && navigator.canShare({ files: filesArray } )) {
+  if (navigator.canShare && navigator.canShare({ files: filesArray })) {
     navigator.share({
       files: filesArray,
       title: 'Check this out',
       text: 'Hello friend\nWhat do you think of this look?\n\nRyan',
     })
-    .then(() => console.log('Share was successful.'))
-    .catch((error) => console.log('Sharing failed', error));
+      .then(() => console.log('Share was successful.'))
+      .catch((error) => console.log('Sharing failed', error));
   } else {
     console.log('Your system doesn\'t support sharing files.');
   }
 }
 
-function cancelImage(){
+function cancelImage() {
   document.getElementById('share-container').style.display = 'none';
-  main() 
-
+  main()
 }
 
 //stop AR
 function stop() {
-  document.getElementById('ar-container').style.display = 'none'; 
+  document.getElementById('ar-container').style.display = 'none';
   JEEFACEFILTERAPI.toggle_pause(true, true)
 }
-
