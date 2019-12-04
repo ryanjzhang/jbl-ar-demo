a650b99857680620d45e3a50d68da37015c26346
Ryan <ryan.j.z@hotmail.com>
2019-12-04 13:34:47 +0100

    Fix missing div, added Vue component
    Fixed missing div in helm, boxen en catalogus html causing bugs.
    Added new  android-content and ios-content vue component

--- vue/components.vue.js
+++ vue/components.vue.js
@@ -1,10 +1,33 @@
 var app = new Vue({
-  el: "#app",
+  el: "#android-content",
   data: {
     message: "Hello Vue!"
   },
   template: `<div class="cat-content">
+    <div class="temp-box">dit is desktop/android :)</div>
+    <div class="temp-box2"></div>
     <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+  </div>`
+});
+
+var app = new Vue({
+  el: "#ios-content",
+  data: {
+    message: "Hello Vue!"
+  },
+  template: `<div class="cat-content">
+    <div class="temp-box">dit is ios :)</div>
     <div class="temp-box2"></div>
     <div class="temp-box"></div>
     <div class="temp-box2"></div>
