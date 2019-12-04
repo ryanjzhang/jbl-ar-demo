0747a729f05dd89da28cf55dac9c855f82e5518a
Ryan <ryan.j.z@hotmail.com>
2019-12-04 13:13:12 +0100

    Added catalogus & implemented start Vue
    Added Vue test component (used by catalogus)
    Added Catalogus to the menu for both mobile and desktop

---
+++ vue/components.vue.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!"
+  },
+  template: `<div class="cat-content">
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+    <div class="temp-box"></div>
+    <div class="temp-box2"></div>
+  </div>`
+});
